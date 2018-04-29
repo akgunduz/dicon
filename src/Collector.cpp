c058b247be297c0625cfd0606bccd33f78286489
Haluk Akgunduz <akgunduz@gmail.com>
2018-04-29 20:59:17 +0300

    ui update

--- src/Collector.cpp
+++ src/Collector.cpp
@@ -98,7 +98,7 @@ bool Collector::processDistributorNodeMsg(long address, Message *msg) {
     LOGS_I(getHost(), getID(), "Available Node : %s",
           InterfaceTypes::getAddressString(nodeAddress).c_str());
 
-    //LOG_U(UI_UPDATE_COLL_PROCESS_LISTITEM, executorIndex, PROCESS_STATE_STARTED);
+    LOG_U(UI_UPDATE_COLL_PROCESS_LISTITEM, executor.getID(), PROCESS_STATE_STARTED);
 
     return send2NodeJobMsg(nodeAddress, msg->getData()->getJobDir(), executor.getID(),
                            executor.get()->getParsedExec(), executor.get()->getFileList());
@@ -125,7 +125,7 @@ bool Collector::processNodeBinaryMsg(long address, Message *msg) {
 
     LOG_U(UI_UPDATE_COLL_FILE_LISTITEM, fileListIDs);
 
-    //LOG_U(UI_UPDATE_COLL_PROCESS_LISTITEM, executorIndex, PROCESS_STATE_ENDED);
+    LOG_U(UI_UPDATE_COLL_PROCESS_LISTITEM, msg->getData()->getExecutorID(), PROCESS_STATE_ENDED);
 
     TypeMD5List md5List;
 
