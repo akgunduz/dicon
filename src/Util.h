24a83a241a38da55aaeb2119475d26880407b522
Haluk Akgunduz <akgunduz@gmail.com>
2020-08-25 22:12:16 +0300

    locale bugfix for linux

--- src/Util.h
+++ src/Util.h
@@ -23,13 +23,13 @@ public:
 
     static inline void ltrim(std::string &s) {
         s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
-            return !std::isspace(ch, std::locale());
+            return !std::isspace(ch);
         }));
     }
 
     static inline void rtrim(std::string &s) {
         s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-            return !std::isspace(ch, std::locale());
+            return !std::isspace(ch);
         }).base(), s.end());
     }
 
@@ -39,7 +39,7 @@ public:
     }
 
     static std::string to_narrow( const wchar_t *s, char dfault = '?',
-                          const std::locale& loc = std::locale())
+                          const std::locale& loc = std::locale::classic())
     {
         std::ostringstream stm;
 
